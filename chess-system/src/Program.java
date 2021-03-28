9248a239ccdc58f08b5583b0adc8c6557d5321de
Henrique <carloshenrique3250@gmail.com>
2021-03-28 10:50:24 -0300

    First class: Position
    created class position

--- chess-system/src/Program.java
+++ chess-system/src/Program.java
@@ -1,5 +1,10 @@
+import Boardgame.Position;
+
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+
     }
 }
